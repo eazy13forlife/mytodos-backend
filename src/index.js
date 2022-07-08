8de61155f1c0afba64b8a4e5b4aa90716862794d
Eric B <eric_baffour-addo@alumni.brown.edu>
2022-07-07 22:38:07 -0700

    removed app.options

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const publicPath = path.join(__dirname, "../public/");
 app.use(
   cors({
     allowedHeaders: ["Content-Type", "authorization", "Accept"],
-    origin: "*",
+    origin: "https://baffour-todos-backend.herokuapp.com",
     preflightContinue: true,
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
   })
@@ -35,9 +35,7 @@ app.use(usersRouter);
 app.use(tasksRouter);
 app.use(notesRouter);
 app.use(projectsRouter);
-app.options("/*", (req, res) => {
-  res.send();
-});
+
 app.listen(port, () => {
   console.log(`Your express server is running on port ${port}.`);
 });
